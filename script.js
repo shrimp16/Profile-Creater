e22e85de350077314538f7cca49bfc6ca133c2e4
Shrimp <lcamara0916@gmail.com>
2022-02-23 15:18:04 -0100

    User can now change the banner picture and the profile picture

--- script.js
+++ script.js
@@ -3,10 +3,15 @@ let aboutMe = document.getElementById("aboutme");
 let button = document.getElementById("editProfile");
 let save = document.getElementById("save");
 
+let userPicChange = document.getElementById("user-pic");
+let userBannerChange = document.getElementById("banner-pic");
+
 $('#editProfile').click(() => {
 
     button.style.display = "none";
     save.style.display = "block";
+    userPicChange.style.display = "block";
+    userBannerChange.style.display = "block";
 
     let currentUsername = username.innerText;
     let currentAboutMe = aboutMe.innerText;
@@ -23,9 +28,31 @@ $('#save').click(() => {
 
     button.style.display = "block";
     save.style.display = "none";
+    userPicChange.style.display = "none";
+    userBannerChange.style.display = "none";
     let newUsername = document.querySelector("#newUsername").value;
     let newAboutMe = document.querySelector("#newAboutMe").value;
 
     username.innerHTML = `<h4>${newUsername}</h4>`;
     aboutMe.innerHTML = `<p>${newAboutMe}</p>`;
 })
+
+let uploaded_image;
+
+userBannerChange.addEventListener('change', function() {
+  const reader = new FileReader();
+  reader.addEventListener('load', () => {
+    uploaded_image = reader.result;
+    document.querySelector("#banner").src = uploaded_image;
+  });
+  reader.readAsDataURL(this.files[0]);
+});
+
+userPicChange.addEventListener('change', function() {
+    const reader = new FileReader();
+    reader.addEventListener('load', () => {
+      uploaded_image = reader.result;
+      document.querySelector("#user").src = uploaded_image;
+    });
+    reader.readAsDataURL(this.files[0]);
+  });
